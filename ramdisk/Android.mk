223ef9d8e2f944450b1409c225bbffd5d7f51d59
enesuzun2002 <emurat.uzun@gmail.com>
2023-02-19 08:49:53 +0300

    zerofltexx: move ramdisk to vendor

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -4,33 +4,35 @@ include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.samsungexynos7420
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= fstab.samsungexynos7420
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH	:=  $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.baseband.rc
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := init.baseband.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.gps.rc
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := init.gps.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.zerofltexx.rc
+LOCAL_MODULE_STEM       := init.target.rc
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := init.zerofltexx.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.samsungexynos7420.rc
+LOCAL_MODULE_STEM   	:= ueventd.rc
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= ueventd.samsungexynos7420.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
