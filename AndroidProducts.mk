49c641f9bf9c35961349995606fd873e851072b2
ghenchant <164819109+ghenchant@users.noreply.github.com>
2024-05-13 18:43:47 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,10 +14,10 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/samsung/zerofltexx
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/lineage_zerofltexx.mk
+    $(LOCAL_DIR)/lineage_zerofltexx.mk
 
 COMMON_LUNCH_CHOICES := \
+    lineage_zerofltexx-eng \
+    lineage_zerofltexx-user \
     lineage_zerofltexx-userdebug
