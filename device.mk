bb06bde4b78b29d605c48d1eb54b31c1e64983ed
ghenchant <164819109+ghenchant@users.noreply.github.com>
2024-05-14 21:38:02 -0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,97 +14,13 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/samsung/zerofltexx
+# Inherit from the common tree
+$(call inherit-product, device/samsung/exynos7420-common/common.mk)
 
-# Permissions
-PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml \
-	$(LOCAL_PATH)/configs/permissions/com.samsung.permission.HRM_EXT.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.samsung.permission.HRM_EXT.xml \
-	$(LOCAL_PATH)/configs/permissions/com.samsung.permission.SSENSOR.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.samsung.permission.SSENSOR.xml
-
-# AAPT
-# This device is 640dpi.  However the platform doesn't
-# currently contain all of the bitmaps at 640dpi density so
-# we do this little trick to fall back to the xxhdpi version
-# if the 640dpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := 640dpi
-# A list of dpis to select prebuilt apk, in precedence order.
-PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
-
-# Audio
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0.xml \
-	$(LOCAL_PATH)/configs/audio/mixer_paths-audience.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_0-audience.xml
-
-# Camera-Shim
-PRODUCT_PACKAGES += \
-	libexynoscamera_shim
-
-# Cpboot-daemon
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/prebuilt/cbd:$(TARGET_COPY_OUT_SYSTEM)/bin/cbd
-
-# Fingerprint-Shim
-PRODUCT_PACKAGES += \
-	libbauthtzcommon_shim
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit from universal7420-common
-$(call inherit-product, device/samsung/universal7420-common/device-common.mk)
-
-# IR
-PRODUCT_PACKAGES += \
-	android.hardware.ir@1.0-impl \
-	android.hardware.ir@1.0-service
-
-# Keylayouts
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
-	$(LOCAL_PATH)/configs/keylayout/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl \
-	$(LOCAL_PATH)/configs/idc/Synaptics_HID_TouchPad.idc:system/usr/idc/Synaptics_HID_TouchPad.idc \
-	$(LOCAL_PATH)/configs/idc/ft5x06_ts.idc:system/usr/idc/ft5x06_ts.idc \
-	$(LOCAL_PATH)/configs/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc
-
-# NFC
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-hal.conf \
-	$(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec.conf \
-	$(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
-
-# OpenMAX-shims
-PRODUCT_PACKAGES += \
-    libui_zero_shim \
-    libExynosOMX_shim
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/android.hardware.media.omx@1.0-service.rc:system/vendor/etc/init/android.hardware.media.omx@1.0-service.rc
-
-
-# Media - Transcoding
-PRODUCT_VENDOR_PROPERTIES += \
-    persist.sys.fuse.transcode_user_control=true \
-    persist.sys.fuse.transcode_enabled=false \
-
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-
-# Ramdisk
-PRODUCT_PACKAGES += \
-	fstab.samsungexynos7420 \
-	init.baseband.rc \
-	init.gps.rc \
-	init.zerofltexx.rc \
-	ueventd.samsungexynos7420.rc
-
-# Sensors
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/sensors/gps.xml:system/etc/gps.xml \
-	$(LOCAL_PATH)/configs/sensors/lhd.conf:system/etc/lhd.conf
-
-# Soong
-PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
-
-# Call the proprietary
+# Inherit proprietary files
 $(call inherit-product, vendor/samsung/zerofltexx/zerofltexx-vendor.mk)
+
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
