6b7fe44a218a905c714815a86aa4c0a4528d1911
ghenchant <164819109+ghenchant@users.noreply.github.com>
2024-06-04 13:54:56 -0500

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -9,19 +9,19 @@
 function blob_fixup() {
     case "${1}" in
         vendor/bin/gpsd)
-            "${PATCHELF}" --replace-needed libgui.so libsensor.so "${BLOB_ROOT}/bin/gpsd"
+            "${PATCHELF}" --replace-needed libgui.so libsensor.so "${BLOB_ROOT}/bin/gpsd "${2}"
             ;;
         vendor/lib/libsec-ril.so)
-            "${PATCHELF}" --replace-needed libprotobuf-cpp-full.so libprotobuf-cpp-fl24.so "${BLOB_ROOT}/vendor/lib/libsec-ril.so"
+            "${PATCHELF}" --replace-needed libprotobuf-cpp-full.so libprotobuf-cpp-fl24.so "${BLOB_ROOT}/vendor/lib/libsec-ril.so "${2}"
             sed -i "s/libprotobuf-cpp-full/libprotobuf-cpp-fl24/" "${BLOB_ROOT}/vendor/lib64/libsec-ril.so"
             ;;
         vendor/lib/libsec-ril-dsds.so)
-            "${PATCHELF}" --replace-needed libprotobuf-cpp-full.so libprotobuf-cpp-fl24.so "${BLOB_ROOT}/vendor/lib/libsec-ril-dsds.so"
+            "${PATCHELF}" --replace-needed libprotobuf-cpp-full.so libprotobuf-cpp-fl24.so "${BLOB_ROOT}/vendor/lib/libsec-ril-dsds.so "${2}"
             sed -i "s/libprotobuf-cpp-full/libprotobuf-cpp-fl24/" "${BLOB_ROOT}/vendor/lib64/libsec-ril-dsds.so"
             ;;
         vendor/lib/hw/nfc_nci.default.so)
-            "${PATCHELF}" --remove-needed vendor.samsung.hardware.nfc@1.0.so "${BLOB_ROOT}/vendor/lib/hw/nfc_nci.default.so"
-            "${PATCHELF}" --remove-needed vendor.samsung.hardware.nfc@1.0.so "${BLOB_ROOT}/vendor/lib64/hw/nfc_nci.default.so"
+            "${PATCHELF}" --remove-needed vendor.samsung.hardware.nfc@1.0.so "${BLOB_ROOT}/vendor/lib/hw/nfc_nci.default.so "${2}"
+            "${PATCHELF}" --remove-needed vendor.samsung.hardware.nfc@1.0.so "${BLOB_ROOT}/vendor/lib64/hw/nfc_nci.default.so "${2}"
             ;;
         vendor/bin/mcDriverDaemon)
             sed -i "s/\/system\/app/\/vendor\/app/g" "${BLOB_ROOT}/vendor/bin/mcDriverDaemon"
